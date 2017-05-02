796a3f3362405147c731f0f74c5e6e0e6c370008
ngryman <ngryman@gmail.com>
2017-05-02 10:20:30 +0200

    :umbrella: fix tests regression

--- test/create-report.js
+++ test/create-report.js
@@ -7,27 +7,27 @@ import pify from 'pify'
 
 // Ours
 import createReport from '../lib/create-report'
-const { readFile } = require('./.internal/util')
+const { readFile } = require('../lib/.internal/util')
 
 const loadTrace = filename =>
-  readFile(filename)
+  readFile(filename, null)
     .then(pify(zlib.gunzip))
     .then(JSON.parse)
 
 test('report profiling', async t => {
-  const trace = await loadTrace('./test/fixtures/high-cpu.trace')
+  const trace = await loadTrace('./test/fixtures/high-cpu.trace.gz')
   const report = await createReport('./test/fixtures/high-cpu.trace', trace)
   t.snapshot(report.profiling)
 })
 
 test('report fps', async t => {
-  const trace = await loadTrace('./test/fixtures/render-loop.trace')
+  const trace = await loadTrace('./test/fixtures/render-loop.trace.gz')
   const report = await createReport('./test/fixtures/render-loop.trace', trace)
   t.snapshot(report.fps)
 })
 
 test('report first paint', async t => {
-  const trace = await loadTrace('./test/fixtures/render-loop.trace')
+  const trace = await loadTrace('./test/fixtures/render-loop.trace.gz')
   const report = await createReport('./test/fixtures/render-loop.trace', trace)
   t.snapshot(report.firstPaint)
 })
