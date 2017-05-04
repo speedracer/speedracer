ec75aaa7a26cb1e3090cd3a042b6e20727e4d249
ngryman <ngryman@gmail.com>
2017-05-04 23:33:14 +0200

    :nut_and_bolt: remove serve.js

--- serve.js
+++
@@ -1,8 +0,0 @@
-const startServer = require('./lib/server')
-const createRunner = require('./lib/runner-server')
-
-startServer({ baseDir: process.cwd(), port: 3000 })
-createRunner({ port: 3001 })
-  .then(runner => {
-    runner.on('run:start', () => console.log('START'))
-  })
