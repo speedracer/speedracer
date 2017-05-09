449d3bcea19ade8ea7d0b9c38ef48a8df7702486
ngryman <ngryman@gmail.com>
2017-05-09 11:41:24 +0200

    :sparkles: (cli) introduce trace command which is the default one

---
+++ bin/speedracer-trace.js
@@ -0,0 +1,161 @@
+#!/usr/bin/env node
+
+// Native
+const path = require('path')
+
+// Packages
+const chalk = require('chalk')
+const mapSeries = require('p-map-series')
+const meow = require('meow')
+const series = require('p-series')
+const waterfall = require('p-waterfall')
+
+// Ours
+const createDirector = require('../lib/director')
+const createDriver = require('../lib/driver')
+const createReporter = require('../lib/reporter')
+const createRunnerServer = require('../lib/runner-server')
+const display = require('../lib/display')
+const launchChrome = require('../lib/chrome-launcher')
+const startServer = require('../lib/server')
+const { flat, forEachProp, pipe } = require('../lib/.internal/util')
+
+const DEBUG = process.env.DEBUG
+
+const defaultFlags = {
+  output: path.join(process.cwd(), '.speedracer'),
+  port: 3000,
+  traces: true,
+  reports: true,
+  runnerPort: 3001
+}
+
+/* eslint-disable */
+const argv = meow({
+  description: false,
+  help: `
+    ${display.logo()}
+
+    ${chalk.red(`speedracer trace ${chalk.underline('files')} [options]`)}
+
+    ${display.section('Options:')}
+
+      -h, --help            Usage information    ${display.subtle(false)}
+      -o ${display.emphasis('dir')}, --output=${display.emphasis('dir')}  Output directory     ${display.subtle('.speedracer')}
+      -p, --port            Tracing server port  ${display.subtle(defaultFlags.port)}
+      --no-traces           Don't save traces    ${display.subtle(!defaultFlags.traces)}
+      --no-reports          Don't save reports   ${display.subtle(!defaultFlags.reports)}
+      --runner-port         Runner server port   ${display.subtle(defaultFlags.runnerPort)}
+
+    ${display.section('Examples:')}
+
+    ${display.subtle('–')} Run files recursively in ${chalk.underline('perf')} directory:
+
+      ${chalk.cyan('$ speedracer perf/**')}
+
+    ${display.subtle('–')} Save only reports to ${chalk.underline('reports')} directory:
+
+      ${chalk.cyan(`$ speedracer perf/*.js --output=reports --no-traces`)}
+  `
+}, {
+  string: ['output'],
+  boolean: ['help', 'traces', 'reports'],
+  alias: {
+    help: 'h',
+    output: 'o',
+    port: 'p'
+  },
+  default: defaultFlags
+})
+/* eslint-enable */
+
+const header = () => console.log('')
+
+const footer = () => console.log('\n')
+
+const cleanup = ({ modules }) =>
+forEachProp(modules, m => { if (m && m.close) m.close() })
+
+const prepare = ({ files, options }) => {
+  if (files.length === 0) {
+    throw new Error('No files to trace found!')
+  }
+}
+
+// TODO: check what happens if one fails, how can we cleanup the others?
+const initialize = baton =>
+series([
+  () => launchChrome(),
+  () => startServer({
+    baseDir: process.cwd(),
+    port: baton.options.port,
+    clientPort: baton.options.runnerPort
+  }),
+  () => createRunnerServer({
+    port: baton.options.runnerPort
+  }),
+  () => createDriver({
+    port: baton.options.port
+  }),
+  () => createReporter(DEBUG ? 'noop' : 'compact')
+]).then(modules => {
+  baton.modules = {
+    chrome: modules[0],
+    server: modules[1],
+    runner: modules[2],
+    driver: modules[3],
+    reporter: modules[4]
+  }
+
+  baton.modules.director = createDirector(baton.modules)
+
+  process.on('SIGINT', () => {
+    cleanup(baton)
+    display.showCursor()
+    process.stdout.write('\n')
+    process.exit()
+  })
+})
+
+const runFiles = ({ files, options, modules }) =>
+waterfall([
+  () => modules.reporter.start(files),
+  () => mapSeries(files, file =>
+    modules.director.runFile(file)
+      .then(runs => mapSeries(runs, run => {
+        if (options.traces) {
+          run.saveTrace(options.output)
+        }
+
+        run.createReport()
+        if (options.reports) {
+          run.saveReport(options.output)
+        }
+        return run
+      }))
+  ),
+  runs => modules.reporter.finish(flat(runs))
+])
+
+const error = (err, baton) => {
+  console.error(chalk.red(err.message))
+  if (DEBUG) console.error(err.stack)
+  cleanup(baton)
+  footer()
+  display.showCursor()
+  process.exit(1)
+}
+
+const run = (files, options) =>
+pipe({ files, options, modules: {}}, [
+  display.hideCursor,
+  header,
+  prepare,
+  initialize,
+  runFiles,
+  cleanup,
+  footer,
+  display.showCursor
+], error)
+
+run(argv.input.slice(1), argv.flags)
