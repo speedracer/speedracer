baee9db304b131c3ce4268af29c71863f40e31c0
ngryman <ngryman@gmail.com>
2017-05-19 11:18:17 +0200

    plugins work

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ const createTracer = require('./lib/tracer')
 const launchChrome = require('./lib/chrome-launcher')
 const startServer = require('./lib/server')
 
+const pluginEvent = (plugins, event) => (...args) => (
+  Promise.all(plugins.map(plugin => {
+    const fn = plugin[event]
+    if (fn) fn.apply(plugin, args)
+  }))
+)
+
 const cleanup = ({ modules }) => (
   eachProp(modules, m => {
     if (m && m.close) m.close()
@@ -26,7 +33,7 @@ const load = baton => (
 )
 
 const initialize = baton => {
-  const { reporters, plugins, config } = baton
+  const { config } = baton
 
   return Promise.all([
     launchChrome(config),
@@ -35,8 +42,18 @@ const initialize = baton => {
   ])
   .then(([ chrome, server, runner ]) => (
     createDriver(config).then(driver => {
+      const { plugins } = baton
+      const tracer = createTracer(runner, driver, config)
+
+      tracer.on('file:start', pluginEvent(plugins, 'onFileStart'))
+      tracer.on('file:finish', pluginEvent(plugins, 'onFileFinish'))
+      tracer.on('race:start', pluginEvent(plugins, 'onRaceStart'))
+      tracer.on('race:finish', pluginEvent(plugins, 'onRaceFinish'))
+      tracer.on('status', pluginEvent(plugins, 'onStatus'))
+      tracer.on('warn', pluginEvent(plugins, 'onWarn'))
+
       baton.modules = { chrome, server, runner, driver }
-      baton.tracer = createTracer({ runner, driver, reporters, plugins, config })
+      baton.tracer = tracer
     })
   ))
 }
