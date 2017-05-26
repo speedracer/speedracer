c8f33b5130ea6731a45174d33f989a8ad79fc905
ngryman <ngryman@gmail.com>
2017-05-26 09:30:08 +0200

    fix modules

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-const Chrome = require('./lib/modules/chrome')
-const ChromeRemote = require('./lib/modules/chrome-remote')
-const Driver = require('./lib/modules/driver')
-const FileServer = require('./lib/modules/file-server')
-const Runner = require('./lib/modules/runner')
-const RunnerServer = require('./lib/modules/runner-server')
 const speedracer = require('./lib/speedracer')
-const Tracer = require('./lib/modules/tracer')
 
-module.exports = options => (
-  speedracer(options, {
-    tracer: [Tracer,
-      [Driver, [ChromeRemote, Chrome]],
-      [Runner, RunnerServer]
-    ],
-    server: FileServer
-  })
-)
+module.exports = speedracer
