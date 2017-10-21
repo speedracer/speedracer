dfc6036377f6865aed4ebc7a1331d0114cd64202
ngryman <ngryman@gmail.com>
2017-10-21 19:21:39 +0200

    :art: (test) destructure first paint tests

--- test/analyzers/first-paint.js
+++ test/analyzers/first-paint.js
@@ -3,13 +3,13 @@ import test from 'ava'
 import analyzeFirstPaint from '../../lib/analyzers/first-paint'
 
 test('compute the first paint time', async t => {
-  const report = await analyzeFirstPaint({
+  const { firstPaint } = await analyzeFirstPaint({
     events: require('../fixtures/artifacts/first-paint.json')
   })
-  t.is(report.firstPaint, 0.027329)
+  t.is(firstPaint, 0.027329)
 })
 
 test('return null is no usable events were found', async t => {
-  const report = await analyzeFirstPaint({ events: [] })
-  t.is(report.firstPaint, null)
+  const { firstPaint } = await analyzeFirstPaint({ events: [] })
+  t.is(firstPaint, null)
 })
