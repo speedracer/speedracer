603c79737cb1f207371c49a415b77ae12bba181e
ngryman <ngryman@gmail.com>
2017-10-21 19:20:55 +0200

    :sparkles: (analyzers) add fps analyzer

---
+++ test/analyzers/fps.js
@@ -0,0 +1,19 @@
+import test from 'ava'
+
+import analyzeFPS from '../../lib/analyzers/fps'
+
+test('compute fps stats', async t => {
+  const { fps } = await analyzeFPS({
+    events: require('../fixtures/artifacts/frames.json')
+  })
+  t.is(fps.min, 31)
+  t.is(fps.max, 33)
+  t.is(fps.avg, 32)
+  t.is(fps.v, 2)
+  t.is(fps.std, 1.41)
+})
+
+test('return null is no usable events were found', async t => {
+  const { fps } = await analyzeFPS({ events: [] })
+  t.is(fps, null)
+})
