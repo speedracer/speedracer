f67025b09af11ec2a80de068f0c623cc8c4f4a2f
ngryman <ngryman@gmail.com>
2017-10-21 17:45:09 +0200

    :hammer: (loaders) now return a string

--- test/loaders/javascript.js
+++ test/loaders/javascript.js
@@ -1,48 +1,48 @@
 import test from 'ava'
 import { spy } from 'sinon'
 
-import RollupLoader from '../../lib/loaders/rollup'
+import JavaScriptLoader from '../../lib/loaders/javascript'
 
 test('load the code of a file', async t => {
-  const loadCode = await RollupLoader()
-  const { code } = await loadCode('test/fixtures/series/no-race.js')
+  const loadCode = await JavaScriptLoader()
+  const code = await loadCode('test/fixtures/series/no-race.js')
   t.snapshot(code)
 })
 
 test('bundle a file using ES6 modules', async t => {
-  const loadCode = await RollupLoader()
-  const { code } = await loadCode('test/fixtures/series/primes.js')
+  const loadCode = await JavaScriptLoader()
+  const code = await loadCode('test/fixtures/series/primes.js')
   t.snapshot(code)
 })
 
 test('accept additional file extensions', async t => {
-  const loadCode = await RollupLoader({ extensions: ['.mjs'] })
-  const { code } = await loadCode('test/fixtures/series/no-race.mjs')
+  const loadCode = await JavaScriptLoader({ extensions: ['.mjs'] })
+  const code = await loadCode('test/fixtures/series/no-race.mjs')
   t.snapshot(code)
 })
 
 test('accept rollup plugins', async t => {
   const plugin = { resolveId: spy() }
-  const loadCode = await RollupLoader({ plugins: [plugin] })
-  const { code } = await loadCode('test/fixtures/series/primes.js')
+  const loadCode = await JavaScriptLoader({ plugins: [plugin] })
+  const code = await loadCode('test/fixtures/series/primes.js')
   t.true(plugin.resolveId.called)
   t.snapshot(code)
 })
 
 test('ignore an empty file', async t => {
-  const loadCode = await RollupLoader()
+  const loadCode = await JavaScriptLoader()
   const res = await loadCode('test/fixtures/series/empty.js')
   t.is(res, null)
 })
 
 test('ignore if a file has a wrong extension', async t => {
-  const loadCode = await RollupLoader()
+  const loadCode = await JavaScriptLoader()
   const res = await loadCode('test/fixtures/series/hello.mjs')
   t.is(res, null)
 })
 
 test('explode if a file does not exists', async t => {
-  const loadCode = await RollupLoader()
+  const loadCode = await JavaScriptLoader()
   const error = await t.throws(loadCode('404.js'))
   t.is(error.code, 'FILE_NOTFOUND')
   t.is(error.message, 'File could not be found')
@@ -50,7 +50,7 @@ test('explode if a file does not exists', async t => {
 })
 
 test('explode if a file contains invalid code', async t => {
-  const loadCode = await RollupLoader()
+  const loadCode = await JavaScriptLoader()
   const error = await t.throws(loadCode('test/fixtures/series/invalid.js'))
   t.is(error.code, 'PARSE_ERROR')
   t.is(error.message, 'Unterminated string constant')
